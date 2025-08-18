f49c4b0ae39e8c0d156e45c24409161a05103dc1
jfallmann <fallmann.joerg@gmail.com>
2025-08-18 12:33:29 +0200

    works

--- R/BRIDGE/R/volcano_ui.R
+++ R/BRIDGE/R/volcano_ui.R
@@ -1,12 +1,12 @@
 #' @export
 VolcanoUI  <- function(id, tbl_name, contrasts) {   
-                        ns <- NS(id)
+                        ns <- NS(id)                        
                         shinydashboard::box(title = "Volcano", width = 12, solidHeader = TRUE, status = "info",
                         shiny::fluidRow(),
                         shiny::fluidRow(
                         shinydashboard::box(title = "Settings", width = 5, solidHeader = T, status = "info",
-                            shinyWidgets::virtualSelectInput(ns(paste0("comparison_volcano_", tbl_name)), "Select Comparison:", choices = NULL),
-                            shiny::selectizeInput(ns(paste0("volcano_search_",tbl_name)), "Search for Gene:", choices = contrasts, selected = contrasts[1], multiple=T),
+                            shinyWidgets::virtualSelectInput(ns(paste0("comparison_volcano_", tbl_name)), "Select Comparison:", choices = contrasts, selected = contrasts[1]),
+                            shiny::selectizeInput(ns(paste0("volcano_search_",tbl_name)), "Search for Gene:", choices = NULL),
                             shinyWidgets::chooseSliderSkin("Flat", color="#3d8dbc"),
                             shiny::numericInput(ns(paste0("volcano_pcutoff_", tbl_name)), "FDR Threshold:", 
                                         min = 0, max = 10, value = 0.05, step = 0.01),
